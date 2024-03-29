1984f896c3d8bc6ad7d7273962fd432608689512
Valerii Dzhalaganiia <dzhvalera199292@gmail.com>
2024-03-29 17:15:50 +0100

    Add test for API request token fro user

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,7 +1,11 @@
 """
 Serializers for the user API View.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
 
 from rest_framework import serializers
 
@@ -17,3 +21,13 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer fro the user token"""
+    email = serializer.EmailField()
+    password = serializer.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
