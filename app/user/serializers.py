6fbce021828a55e3a5d5abd914eb7b1e9c0983c4
Valerii Dzhalaganiia <dzhvalera199292@gmail.com>
2024-03-30 18:14:40 +0100

    Add authontication tests for user

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -26,8 +26,24 @@ class UserSerializer(serializers.ModelSerializer):
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer fro the user token"""
-    email = serializer.EmailField()
-    password = serializer.CharField(
+    email = serializers.EmailField()
+    password = serializers.CharField(
         style={'input_type': 'password'},
         trim_whitespace=False,
     )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msq = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
